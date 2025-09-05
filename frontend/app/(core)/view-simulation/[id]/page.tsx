15f6f95fcb5d8278923e4c74b51b12fd8a56442d
John Yeung <37952085+lt-shy-john@users.noreply.github.com>
2025-09-05 15:13:08 +1000

    Finish basic function of viewing simulation settings

--- frontend/app/(core)/view-simulation/[id]/page.tsx
+++ frontend/app/(core)/view-simulation/[id]/page.tsx
@@ -1,40 +1,36 @@
 "use client";
 
-import { use, useEffect, useState } from 'react';
-import { useForm } from "react-hook-form";
-import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { useRouter, useParams } from 'next/navigation';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-type CreateSimulationData = {
-    numberOfAgent: number,
-    simulationPeriod: number,
-    createdBy: {
-        username: string
-    }
-}
-
-async function getSimuData(id: number) {
-    const res = await fetch("http://localhost:8000/simulations/" + id);
-    if (!res.ok) {
-        console.log("Failed to fetch data");
-        throw new Error("Failed to fetch data");
-    }
-}
-
 export default function Page({ params, }: { params: Promise<{ id: string }> }) {
     const router = useRouter();
-    const { id } = use(params);
+    const { id } = useParams();
+    console.log('Entered view simulation page for ' + id + ".");
 
-  return (
-      <div id='title'>
-          <Typography variant="h3">Simulation</Typography>
-          <Typography variant="h6">ID</Typography>
-          <Typography variant="body1">{id}</Typography>
-          <Typography variant="h6">Number of agents (N)</Typography>
-          <Typography variant="body1">{/*<p>{formData['numberOfAgent']}</p>*/}</Typography>
-          <Typography variant="h6">Simulation time (T)</Typography>
-          <Typography variant="body1">{/*<p>{formData['simulationPeriod']}</p>*/}</Typography>
+    const [loading, setLoading] = useState(true);
+    const [simuData, setSimuData] = useState({});
+
+    useEffect(() => {
+        fetch("http://localhost:8000/simulations/" + id).then(response => response.json()).then(json => setSimuData(json));
+        setLoading(false);
+    }, []);
+
+    return (
+        <div id='title'>
+            <Typography variant="h3">Simulation</Typography>
+            {loading ? (<Typography variant="body1">Loading...</Typography>) : (
+                <>
+                    <Typography variant="h6">ID</Typography>
+                    <Typography variant="body1">{id}</Typography>
+                    <Typography variant="h6">Number of agents (N)</Typography>
+                    <Typography variant="body1">{simuData.numberOfAgent}</Typography>
+                    <Typography variant="h6">Simulation time (T)</Typography>
+                    <Typography variant="body1">{simuData.simulationPeriod}</Typography>
+                </>
+            ) }
     </div>
-  )
+    )
 }

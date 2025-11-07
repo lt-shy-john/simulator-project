2e4c43154333f78532f1a546b620be1baf99bacf
John Yeung <37952085+lt-shy-john@users.noreply.github.com>
2025-11-07 20:59:43 +1100

    Moved managing simulations into a subfolder of it's kind

--- frontend/app/(docs)/docs/layout.tsx
+++ frontend/app/(docs)/docs/layout.tsx
@@ -6,7 +6,6 @@ import { baseOptions } from '../../layout.config'
 
 export default function Layout({ children }: { children: ReactNode }) {
     return (
-        
             <DocsLayout tree={source.pageTree} {...baseOptions}>
                 {children}
             </DocsLayout>

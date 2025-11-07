ecb6ec2d278ce7344827be58a0cff446eca29899
John Yeung <37952085+lt-shy-john@users.noreply.github.com>
2025-11-07 17:02:18 +1100

    Complete the doc page

--- frontend/.source/source.config.mjs
+++ frontend/.source/source.config.mjs
@@ -3,6 +3,7 @@ import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 import { defineDocs, defineConfig } from "fumadocs-mdx/config";
 var source_config_default = defineConfig({
+  lastModifiedTime: "git",
   mdxOptions: {
     remarkPlugins: [remarkMath],
     // Place it at first, it should be executed before the syntax highlighter

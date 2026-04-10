f2207b6263d8c526181e72fe936556efdb58f3c9
John Yeung <37952085+lt-shy-john@users.noreply.github.com>
2026-04-10 12:23:35 +1000

    Add logging capability

--- simulator-project-simulator/simulator.py
+++ simulator-project-simulator/simulator.py
@@ -1,13 +1,19 @@
 import sys
+import datetime
 
 from runner.simulation import Simulation
+import runner.customLogger as logger
+
+filename = 'Test-' + datetime.datetime.now().strftime("%Y%m%d")
+root_logger = logger.gen_logging(filename, None)
 
 try:
+    N = int(sys.argv[1])
     T = int(sys.argv[2])
 except IndexError:
     print("No simulation time specified.")
     T = 10
 
 if sys.argv[-1] == 'run':
-    simulation = Simulation(10, T)
+    simulation = Simulation(N, T, root_logger, name=filename)
     simulation()

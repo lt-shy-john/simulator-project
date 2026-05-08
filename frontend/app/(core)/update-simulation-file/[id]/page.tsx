f8f687a4ead493a97a155aaa2f9c5b468b58c800
John Yeung <37952085+lt-shy-john@users.noreply.github.com>
2026-05-08 13:40:42 +1000

    Add new page for file creation/ update

---
+++ frontend/app/(core)/update-simulation-file/[id]/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import { useForm, Controller } from "react-hook-form";
+import { useRouter, useParams } from 'next/navigation';
+import { Button, FormControl, Typography, Select, MenuItem, } from '@mui/material';
+
+type Inputs = {
+    name: string, 
+    numberOfAgent: number,
+    simulationPeriod: number,
+    logLevel: number,
+};
+
+export default function Page({ params, }: { params: Promise<{ id: string }> }) {
+    const { register, control, handleSubmit, watch, setValue } = useForm<Inputs>({defaultValues: {
+      logLevel: 'info',
+    },}
+    );
+    const router = useRouter();
+    const { id } = useParams();
+
+    const [simuData, setSimuData] = useState({});
+
+    console.log(watch());
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const savedData = sessionStorage.getItem('data');
+            console.log('Obtained previous form data. ');
+            console.log(savedData);
+            if (savedData) {
+                const data = JSON.parse(savedData);
+                if (data.name) setValue('name', data.name);
+                if (data.numberOfAgent) setValue('numberOfAgent', data.numberOfAgent);
+                if (data.simulationPeriod) setValue('simulationPeriod', data.simulationPeriod);
+            }
+        }
+    }, [setValue]);
+
+    const handleCancelUpdate = () => {
+        console.log("Cancelling simulation update. Removing session item...");
+        sessionStorage.removeItem('data');
+        router.push("/simulation");
+    };
+
+    const onSubmit = (data: Inputs) => {
+        const existing = sessionStorage.getItem('data');
+        const formData = existing ? JSON.parse(existing) : {};
+        sessionStorage.setItem('data', JSON.stringify({ ...formData, ...data }));
+        console.log(sessionStorage);
+        router.push('/update-simulation-confirm/'+simuData.id);
+    };
+
+    return (
+        <div id='title'>
+            <Typography variant="h3">Update Simulation</Typography>
+            <Typography variant="h4">File</Typography>
+            <Typography variant="body2">Create files exported from simulation</Typography>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <fieldset>
+                    <FormControl fullWidth margin="normal">
+                        <label htmlFor="Name"><Typography variant="body1">Log level</Typography></label>
+                        <Controller
+                          name="logLevel"
+                          control={control}
+                          render={({ field }) => (
+                            <Select
+                              {...field}
+                              labelId="log-level-label"
+                              label="Log Level"
+                            >
+                              <MenuItem value="debug">DEBUG</MenuItem>
+                              <MenuItem value="info">INFO</MenuItem>
+                              <MenuItem value="debug">WARNING</MenuItem>
+                              <MenuItem value="error">ERROR</MenuItem>
+                            </Select>
+                          )}
+                        />
+                    </FormControl>
+                    <Button onClick={handleCancelUpdate}>Cancel</Button>
+                    <Button onClick={() => router.back()}>Back</Button>
+                    <Button type="submit" variant="contained">Next</Button>
+                </fieldset>
+            </form>
+        </div>
+    )
+}

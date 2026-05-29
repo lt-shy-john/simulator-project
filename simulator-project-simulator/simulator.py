5ae33608c443c0b51de509035d306b31870e6335
John Yeung <37952085+lt-shy-john@users.noreply.github.com>
2026-05-29 16:03:08 +1000

    First attempt on making the simulator

--- simulator-project-simulator/simulator.py
+++ simulator-project-simulator/simulator.py
@@ -7,17 +7,28 @@ import runner.customLogger as logger
 filename = 'Test-' + datetime.datetime.now().strftime("%Y%m%d")
 root_logger = logger.gen_logging(filename, None)
 
-try:
-    N = int(sys.argv[1])
-except IndexError:
-    print("No simulation agent number specified.")
-    N = 3
-try:
-    T = int(sys.argv[2])
-except IndexError:
-    print("No simulation agent time specified.")
-    T = 10
+# Express mode
+if len(sys.argv) > 2:
+    try:
+        N = int(sys.argv[1])
+    except IndexError:
+        print("No simulation agent number specified.")
+        N = 3
+    try:
+        T = int(sys.argv[2])
+    except IndexError:
+        print("No simulation agent time specified.")
+        T = 10
 
-if sys.argv[-1] == 'run':
-    simulation = Simulation(N, T, root_logger, name=filename)
-    simulation()
+    if sys.argv[-1] == 'run':
+        simulation = Simulation(N, T, root_logger, name=filename)
+        simulation()
+
+# Normal interface
+
+cmd = ""
+
+print('Welcome to the Agent Based Simulator!')
+
+while cmd.lower() not in ["exit", "quit"]:
+    cmd = input('> ')

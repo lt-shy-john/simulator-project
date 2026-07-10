e795deb076f95aebd0fdf8b895d0ca369193b564
John Yeung <37952085+lt-shy-john@users.noreply.github.com>
2026-07-10 13:24:42 +1000

    Added new files

---
+++ simulator-project-simulator/tests/test_soa.py
@@ -0,0 +1,21 @@
+import pytest
+
+from runner.soa import to_soa, from_soa, add_agent, remove_agent
+
+def test_to_soa_success():
+    pass
+
+def test_from_soa_success():
+    pass
+
+def test_soa_add_agent_success():
+    pass
+
+def test_soa_remove_agent_success():
+    pass
+
+def test_soa_remove_last_agent_removes_type_entry():
+    pass
+
+def test_soa_remove_inexistent_agent_throws_exception():
+    pass
